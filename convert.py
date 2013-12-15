8410ebc20e91bddadb2b8ee6d49ffde4e3f1d54d
Jetuvei <iamjustinx@gmail.com>
2013-12-15 15:25:34 +0000

    added doc and tests to convert.py

--- convert.py
+++ convert.py
@@ -21,6 +21,7 @@ __doc__=r"""Command line arguments:
    
 
 import sys
+import bases
 
 # converting TO base 10:--------------------------
 
@@ -75,6 +76,13 @@ number.
 # Converting FROM base 10:---------------------
 
 def findMaxPower(n_base10, new_basis_size):
+    r"""Find largest power of the new basis size that is less 
+than the base 10 number. For example:
+
+12 in base 2 is 1*2^3 + 1*2^2 + 0*2^1 + 0*2^0
+
+The largest power is 3, and this should be returned,
+"""
     
     power = 0
     while new_basis_size ** power < n_base10:
@@ -83,6 +91,11 @@ def findMaxPower(n_base10, new_basis_size):
     return power
 
 def getCharValList(n_base10, new_basis_size):
+    r"""Return a list of integers, where the integers are
+the multiple of the corresponding power of the basis size.
+
+e.g. 175 in base 16 should return [10,15]
+"""
     
     max_power = findMaxPower(n_base10, new_basis_size)
     num = n_base10
@@ -98,6 +111,13 @@ def getCharValList(n_base10, new_basis_size):
     return cvl
 
 def convertFromBase10(n_base10, basis):
+    r"""Should return a representation string of the base
+10 integer in the new basis.
+
+e.g. 175 in base 16 should return "AF"
+
+this function uses getCharValList(...)
+"""
 
     charValList = getCharValList(n_base10, basis.getSize())
 
@@ -115,6 +135,7 @@ def convertFromBase10(n_base10, basis):
 
 def main():
     """UNDER CONSTRUCTION"""
+    pass
 
 # TESTS
 
@@ -123,6 +144,18 @@ def test_findMaxPower_1():
 
 def test_findMaxPower_2():
     assert findMaxPower(15,2) == 3
+
+def test_createBase10List_1():
+    assert createBase10List("1A", bases.BASE16) == [1,10]
+
+def test_convertFromBase10_1():
+    assert convertFromBase10(8,bases.BASE2) == "1000"
+
+def test_convertToBase10_1():
+    assert convertToBase10("AF", bases.BASE16) == 175
+
+def test_getCharValList_1():
+    assert getCharValList(175, bases.BASE16) == ["A","F"]
 
 # run from cmd line    
 
